653ea70966d64e181c81bd93cb22e5fbed41eaa8
Gabriel Gomes <monseha@hotmail.com>
2023-01-27 20:22:37 -0300

    First commit

---
+++ main.tf
@@ -0,0 +1,63 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+# Configure the DigitalOcean Provider
+provider "digitalocean" {
+  token = var.do_token
+}
+
+resource "digitalocean_droplet" "jenkins" {
+  image    = "ubuntu-22-04-x64"
+  name     = "jenkins"
+  region   = var.region
+  size     = var.size
+  ssh_keys = [data.digitalocean_ssh_key.ssh_key.id]
+}
+
+data "digitalocean_ssh_key" "ssh_key" {
+  name = var.ssh_key
+}
+
+resource "digitalocean_kubernetes_cluster" "k8s" {
+  name   = "k8s"
+  region = var.region
+  # Grab the latest version slug from `doctl kubernetes options versions`
+  version = "1.25.4-do.0"
+
+  node_pool {
+    name       = "default"
+    size       = var.size
+    node_count = 2
+  }
+}
+
+output "droplet_ip" {
+    value = digitalocean_droplet.jenkins.ipv4_address
+}
+
+resource "local_file" "foo" {
+  content  = digitalocean_kubernetes_cluster.k8s.kube_config.0.raw_config
+  filename = "kube_config.yaml"
+}
+
+variable "do_token" {
+  default = ""
+}
+
+variable "ssh_key" {
+  default = ""
+}
+
+variable "region" {
+  default = ""
+}
+
+variable "size" {
+  default = ""
+}
---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+do_token = "dop_v1_fe10c5874addb82885f56fead244b0022d67cc04e2afbce1cc2d9b3b66e1ebea"
+ssh_key  = "Terraform"
+region   = "nyc1"
+size = "s-2vcpu-2gb"
